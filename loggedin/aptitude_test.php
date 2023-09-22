175b04f5a60be7e5db626c00a9caaef5d41f72cd
Shreya Gupta <guptaashreya24@gmail.com>
2023-09-22 16:34:02 +0530

    first commit

---
+++ loggedin/aptitude_test.php
@@ -0,0 +1,11 @@
+<!-- Example question -->
+<div>
+  <h2>Question 1:</h2>
+  <p>What is the capital of France?</p>
+  <input type="radio" name="q1" value="a"> a) Paris<br>
+  <input type="radio" name="q1" value="b"> b) London<br>
+  <input type="radio" name="q1" value="c"> c) Rome<br>
+  <input type="radio" name="q1" value="d"> d) Madrid<br>
+</div>
+
+<!-- Add more questions here -->
